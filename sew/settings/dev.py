65b7590fbfe25657c850d609dd3010a36e2f057a
Raphiqui <raphael.pastre@gmail.com>
2024-01-28 10:38:37 +0100

    first commit

---
+++ sew/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-m&1=gg-r0p7#f#@$3(918u_xprt_glb*_c-7)w_7@@to-qor1+"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
