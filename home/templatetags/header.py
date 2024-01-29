c34807fe6b8d1e8f0fefb93bfad56a4f4293a96a
Raphiqui <raphael.pastre@gmail.com>
2024-01-29 21:39:17 +0100

    WIP: menu items come from Wagtail

---
+++ home/templatetags/header.py
@@ -0,0 +1,17 @@
+from django import template
+from wagtail.models import Site
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_menu_items(context):
+    """
+    Wagtail has a specific property for pages that are in the menu called "in_menu"
+    """
+
+    home_page = Site.find_for_request(context.request).root_page
+    menu_items = home_page.get_children().in_menu().live().specific()
+    menu = [home_page.title] + [item.title for item in menu_items]
+
+    return menu
