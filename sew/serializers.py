104e32a523535e12cd4a6ea761683b7ed15411d3
Raphiqui <36623218+Raphiqui@users.noreply.github.com>
2024-02-13 22:29:54 +0100

    WIP: not usefull here

--- sew/serializers.py
+++ sew/serializers.py
@@ -3,28 +3,6 @@ from rest_framework import serializers
 from home.models import SharkPage
 
 
-class SharkPreviewSerializer(serializers.ModelSerializer):
-    """
-    Serializer for Shark model.
-    Only the title and the scientific name are interesting, basically aims to be displayed into sharks page.
-    """
-
-    thumbnail = serializers.SerializerMethodField("get_thumbnail_rendition")
-
-    def get_thumbnail_rendition(self, obj):
-        """
-        get_renditions methods allow to update an image without impacting the quality
-        """
-
-        return obj.image.get_renditions(
-            "width-600", "height-400", "fill-300x186|jpegquality-60"
-        )
-
-    class Meta:
-        model = SharkPage
-        fields = ["title", "scientific_name", "thumbnail"]
-
-
 class SharkSerializer(serializers.ModelSerializer):
     """
     Global serializer for Shark model.
