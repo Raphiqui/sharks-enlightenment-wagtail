2df3ac7391d4e276a6e238e6165afcc71b3271ce
Raphiqui <36623218+Raphiqui@users.noreply.github.com>
2024-02-18 19:38:47 +0100

    serializer testing

--- sew/test/test_serializer.py
+++ sew/test/test_serializer.py
@@ -1,15 +1,16 @@
 import logging
 import pytest
-from django.test import TestCase
-from model_bakery import baker
 
-from home.models import SharkPage
+from home.models import SharkPage, SharksPage
+from wagtail.models import Page, Locale
+from wagtail.tests.utils import WagtailPageTestCase
+
+from home.serializers import SharkPreviewSerializer
 
 logger = logging.getLogger(__name__)
 
 
-class TestSerializer(TestCase):
-    """ """
+class TestSerializer(WagtailPageTestCase):
 
     @pytest.mark.django_db
     def setUp(self):
@@ -17,9 +18,69 @@ class TestSerializer(TestCase):
         pytest allows to create a fake db to populate it with some mockedup data
         """
 
-        # self.shark = baker.make(SharkPage)
-        print("fooo")
+        locale = Locale.objects.create(language_code="en")
 
-    def test_serializer_is_valid(self):
-        value = True
-        assert bool(value) is True
+        Page.objects.create(title="Root", path="0001", depth=1, locale=locale)
+
+        SharksPage.objects.create(
+            title="Sharks", path="00010001", depth=2, locale=locale
+        )
+
+        SharkPage.objects.create(
+            title="Great White Shark",
+            description="The great white shark, also known as the great white, white shark or white pointer, is a species of large mackerel shark which can be found in the coastal surface waters of all the major oceans.",
+            scientific_name="Carcharodon carcharias",
+            distribution="The great white shark is mainly found in the coastal waters of all the major oceans.",
+            path="000100010001",
+            depth=3,
+            locale=locale,
+        )
+
+        SharkPage.objects.create(
+            title="Whale Shark",
+            description="The whale shark is a slow-moving, filter-feeding carpet shark and the largest known extant fish species.",
+            scientific_name="Rhincodon typus",
+            distribution="The whale shark is found in open waters of the tropical oceans and is rarely found in water below 21 °C.",
+            path="000100010002",
+            depth=3,
+            locale=locale,
+        )
+
+    def test_serializer_is_truthy(self):
+        shark_page = SharkPage.objects.first()
+        serializer = SharkPreviewSerializer(instance=shark_page)
+        assert serializer.data is not None
+
+    def test_serializer_has_correct_fields(self):
+        shark_page = SharkPage.objects.first()
+        serializer = SharkPreviewSerializer(instance=shark_page)
+        assert "id" in serializer.data
+        assert "title" in serializer.data
+        assert "scientific_name" in serializer.data
+        assert "thumbnail" in serializer.data
+
+    def test_serializer_thumbnail_may_be_empty(self):
+        shark_page = SharkPage.objects.first()
+        serializer = SharkPreviewSerializer(instance=shark_page)
+        assert serializer.data["thumbnail"] == ""
+
+    def test_serializer_is_empty(self):
+        """
+        There is nothing into the instance so throw an error when accessing data
+        """
+        serializer = SharkPreviewSerializer()
+        assert serializer.data == {"id": None, "scientific_name": "", "title": ""}
+
+    def test_serializer_many_is_truthy(self):
+        shark_pages = SharkPage.objects.all()
+        serializer = SharkPreviewSerializer(instance=shark_pages, many=True)
+        assert serializer.data is not None
+
+    def test_serializer_is_falsy(self):
+        """
+        Try to serialize an instance of a page which is not a SharksPage
+        """
+
+        serializer = SharkPreviewSerializer(instance=SharksPage.objects.first())
+        with self.assertRaises(AttributeError):
+            serializer.data
